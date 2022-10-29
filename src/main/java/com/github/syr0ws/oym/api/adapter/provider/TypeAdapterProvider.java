4cd792d376abb5b167f310d221e9f949b01cb745
= <=>
2022-10-29 22:45:01 +0200

    Adding javadoc.

--- src/main/java/com/github/syr0ws/oym/api/adapter/provider/TypeAdapterProvider.java
+++ src/main/java/com/github/syr0ws/oym/api/adapter/provider/TypeAdapterProvider.java
@@ -5,7 +5,18 @@ import com.github.syr0ws.oym.api.adapter.TypeAdapterFactory;
 
 public interface TypeAdapterProvider<T> {
 
+    /**
+     * Instantiate a TypeAdapter which is able to manipulate objects of type passed as parameter.
+     * @param type the type to be manipulated by the TypeAdapter instance.
+     * @param factory a TypeAdapterFactory instance. It is especially used to retrieve adapters for generic types.
+     * @param generics If the type to manipulate has generic parameters, they must be specified in this field.
+     * @return the corresponding TypeAdapter instance.
+     */
     TypeAdapter<T> provide(Class<T> type, TypeAdapterFactory factory, Class<?>... generics);
 
+    /**
+     * Get the object type manipulated by the provider.
+     * @return an object type.
+     */
     Class<? super T> getType();
 }
--- src/main/java/com/github/syr0ws/oym/api/adapter/provider/TypeAdapterProviderModel.java
+++ src/main/java/com/github/syr0ws/oym/api/adapter/provider/TypeAdapterProviderModel.java
@@ -7,13 +7,38 @@ import java.util.Optional;
 
 public interface TypeAdapterProviderModel {
 
+    /**
+     * Register a new TypeAdapterProvider instance to the model. If the type
+     * manipulated by the instance already exists, it is overridden by the new one.
+     * @param provider an instance of TypeAdapterProvider.
+     */
     void addProvider(@NotNull TypeAdapterProvider<?> provider);
 
+    /**
+     * Unregister a TypeAdapterProvider instance from the model.
+     * @param type an object type manipulated by the instance to be removed.
+     * @return true if a corresponding instance exists and has been removed or else false.
+     */
     boolean removeAdapter(@NotNull Class<?> type);
 
+    /**
+     * Check if a TypeAdapterProvider instance that manipulates a specific object type is registered.
+     * @param type an object type.
+     * @return true if a corresponding instance exists or else false.
+     */
     boolean hasAdapter(@NotNull Class<?> type);
 
+    /**
+     * Get an Optional that contains a TypeAdapterProvider instance that manipulates a specific object type.
+     * @param type an object type.
+     * @return a non-empty optional if an instance of the class TypeAdapterProvider that manipulates
+     * the specified object type is registered or else an empty optional.
+     */
     <T> Optional<TypeAdapterProvider<T>> getAdapter(@NotNull Class<T> type);
 
+    /**
+     * Get all the registered TypeAdapterProvider instances.
+     * @return a collection of TypeAdapterProvider objects.
+     */
     Collection<TypeAdapterProvider<?>> getAdapters();
 }
