3a38113edfddba473c49d3421436611d89fafe9a
= <=>
2022-10-30 22:01:08 +0100

    Adding javadoc.

--- src/main/java/com/github/syr0ws/oym/api/section/ConfigurationSectionException.java
+++ src/main/java/com/github/syr0ws/oym/api/section/ConfigurationSectionException.java
@@ -1,5 +1,8 @@
 package com.github.syr0ws.oym.api.section;
 
+/**
+ * Exception raised when an error occurs using a ConfigurationSection.
+ */
 public class ConfigurationSectionException extends Exception {
 
     public ConfigurationSectionException(String message) {
